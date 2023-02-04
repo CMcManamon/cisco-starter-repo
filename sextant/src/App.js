6bc0fce2748a86a79d0a61ad201497d395a8d497
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2023-02-03 17:42:48 -0800

    Add Address component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import Exhibit from "./components/Exhibit";
+import Address from "./components/Address";
 
 function App() {
   return (
     <div className="App">
       <Banner title={"Sextant"} />
-      <Exhibit heading={"Heading"} children={null}></Exhibit>
+      <Exhibit heading={"IP Address"} children={null}>
+        <Address method="IPv4"></Address>
+        <Address method="IPv6"></Address>
+      </Exhibit>
     </div>
   );
 }
---
+++ sextant/src/components/Address.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+// API URLs, response in json format
+const URL_IPv4 = "https://api.ipify.org?format=json";
+const URL_IPv6 = "https://api64.ipify.org?format=json";
+
+const Address = (props) => {
+  const { method } = props;
+  let [address, setAddress] = useState("Unknown");
+
+  useEffect(() => {
+    let API_URL;
+    if (method === "IPv4") {
+      API_URL = URL_IPv4;
+    } else if (method === "IPv6") {
+      API_URL = URL_IPv6;
+    } else {
+      setAddress("Invalid method");
+      return;
+    }
+
+    // Get the address from ipify
+    setAddress("Checking...");
+    fetch(API_URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => setAddress(method + ": " + data.ip))
+      .catch((error) => {
+        setAddress("Error fetching IP");
+        throw error;
+      })
+      .finally(() => {});
+  }, [method]);
+  return <div className="IPAddress">{address}</div>;
+};
+export default Address;
